fe8423941162d10035d9b73f5bab81c93fe51e23
halmk <elle2206@i.softbank.jp>
2019-05-06 22:45:12 +0900

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -6,4 +6,25 @@ app = Flask(__name__)
 def home():
     res = "This page is HOME!"
     return res
-    
+
+@app.route('/login')
+def login():
+    res = "This page is Login page!"
+    return res
+
+@app.route('/create')
+def createContest():
+    res = "This page is for creating contest!"
+    return res
+
+@app.route('/history')
+def history():
+    res = "This page is for history of participated contest!"
+    return res
+
+@app.route('/contest/<contestID>', methods=["GET","POST"])
+def contest(contestID):
+    return render_template("contest.html", title=contestID,id=contestID)
+
+if __name__ == "__main__":
+    app.run(debug=True)
