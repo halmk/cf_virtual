8d54a845b00394d21b4de5ba6db1835b2fff94b6
halmk <elle2206@i.softbank.jp>
2019-05-08 18:34:02 +0900

    create post update

--- main.py
+++ main.py
@@ -1,21 +1,41 @@
-from flask import Flask, render_template
+from flask import *
+
+import hashlib
+import datetime
+import problem as pm
+import user as usr
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    res = "This page is HOME!"
-    return res
+    return render_template('template.html')
 
 @app.route('/login')
 def login():
-    res = "This page is Login page!"
-    return res
+    return render_template('login.html')
 
-@app.route('/create')
+@app.route('/create', methods=["GET", "POST"])
 def createContest():
-    res = "This page is for creating contest!"
-    return res
+    print("create contest page.")
+    if request.method == "GET":
+        return render_template("create.html")
+    else:
+        contest_name = request.form["contest_name"]
+        start_date = request.form["start_date"]
+        start_time = request.form["start_time"]
+        end_date = request.form["end_date"]
+        end_time = request.form["end_time"]
+        problems_url = request.form["problem_url"]
+
+        print(contest_name)
+        print(start_date)
+        print(start_time)
+        print(end_date)
+        print(end_time)
+        print(problems_url)
+
+        return render_template("create.html")
 
 @app.route('/history')
 def history():
--- problem.py
+++ problem.py
@@ -10,7 +10,7 @@ class Problem(Base):
     """
     __tablename__ = "problems"
     id = Column('id', Integer, primary_key = True)
-    problem = Column('problem', String(100))
+    #problem = Column('problem', String(100))
     problemURL = Column('url', String(100))
     participant = ('participant', String(100))
     contest = Column('contest', String(100))
